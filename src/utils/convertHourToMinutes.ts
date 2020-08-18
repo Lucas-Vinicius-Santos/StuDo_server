9951b3baf661cbf3a8cc534fbe2732ac936785d7
Lucas Santos <lucasparaipaba113@gmail.com>
2020-08-18 18:51:05 -0300

    added utils: function convertHourInMinutes

---
+++ src/utils/convertHourToMinutes.ts
@@ -0,0 +1,8 @@
+
+export default function convertHourToMinutes(number: string) {
+  const [hour, minutes] = number.split(':').map(Number);
+
+  const timeInMinutes = (hour * 60) + minutes;
+
+  return timeInMinutes;
+}
